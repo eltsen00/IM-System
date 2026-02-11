eb1e6619dc288bbbaec77b83c8bc6f4d35dbafe5
Eltsen <eltsen00@gmail.com>
2026-02-11 21:35:27 +0800

    添加用户结构体及相关功能，完善服务器用户管理

--- server.go
+++ server.go
@@ -3,23 +3,53 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	IP   string
 	Port int
+	// 用户在线映射表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex // 读写协同锁，保证在线用户映射表的并发安全
+	// 消息广播的channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
 	server := &Server{
-		IP:   ip,
-		Port: port,
+		IP:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return server
 }
 
+func (this *Server) ListenMessager() {
+	for msg := range this.Message {
+		this.mapLock.RLock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.RUnlock()
+	}
+}
+
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ": " + msg
+	this.Message <- sendMsg
+}
+
 func (this *Server) Handler(conn net.Conn) {
-	fmt.Println("Connection Established")
+	// 处理用户连接
+	user := NewUser(conn)
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播用户上线消息
+	this.BroadCast(user, "已上线")
 }
 
 func (this *Server) Start() {
@@ -28,10 +58,16 @@ func (this *Server) Start() {
 		fmt.Println(err)
 		return
 	}
-	defer listener.Close()
+	defer listener.Close() // 关闭监听器
+
+	// 启动监听Message的goroutine
+	go this.ListenMessager()
+
+	fmt.Printf("服务器已启动，监听 %s:%d\n", this.IP, this.Port)
 
 	for {
 		conn, err := listener.Accept()
+
 		if err != nil {
 			fmt.Println(err)
 			continue
